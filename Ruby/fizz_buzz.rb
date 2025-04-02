59a1690df69413f6ad3351cb45988db013693ff8
Hayashibara <create.manage.mov@gmail.com>
2025-04-02 18:41:33 +0900

    fizz_buzz orizin

---
+++ Ruby/fizz_buzz.rb
@@ -0,0 +1,18 @@
+def fizz_buzz(number)
+    return puts "0以外の数値を入力してください" if number == 0
+    
+    if number % 3 == 0 && number % 5 == 0
+        puts "Fizz Buzz"
+    elsif number % 5 == 0
+        puts "Buzz"
+    
+    elsif number % 3 == 0
+        puts "Fizz"
+    else
+        puts number
+    end
+    
+end
+
+input = gets.to_i
+fizz_buzz(input)
